48d3affd23ad6f5bd94f652e9cdef557d1b1b4a4
yaroslav_yudi <yaskih@mail.ru>
2021-09-09 12:04:14 +0300

    init

---
+++ main.py
@@ -0,0 +1,45 @@
+from pynput.mouse import Button, Controller
+import time
+import random
+
+def test(mouse):
+    # Read pointer position
+    print('The current pointer position is {0}'.format(
+        mouse.position))
+
+    # Set pointer position
+    mouse.position = (10, 20)
+    print('Now we have moved it to {0}'.format(
+        mouse.position))
+
+    # Move pointer relative to current position
+    mouse.move(5, -5)
+
+    # Press and release
+    mouse.press(Button.left)
+    mouse.release(Button.left)
+
+    # Double click; this is different from pressing and releasing
+    # twice on Mac OSX
+    mouse.click(Button.left, 2)
+
+    # Scroll two steps down
+    mouse.scroll(0, 2)
+
+def click(mouse):
+    # Press and release
+    mouse.press(Button.left)
+    mouse.release(Button.left)
+
+def loop(mouse):
+    interval = random.randint(3, 10) / 2
+    click(mouse)
+    time.sleep(interval)
+
+def main():
+    mouse = Controller()
+    while True:
+        loop(mouse)
+
+if __name__ == "__main__":
+    main()
