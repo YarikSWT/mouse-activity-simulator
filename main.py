c84d6edfff906f9e3665ad6c5a323719532b93a6
Vladislav Levchenko <levchenkoo.vladislav@gmail.com>
2022-06-21 08:41:02 +0300

    scroll support

--- main.py
+++ main.py
@@ -27,25 +27,46 @@ def test(mouse):
     # Scroll two steps down
     mouse.scroll(0, 2)
 
+
 def click(mouse):
     # Press and release
     mouse.press(Button.left)
     mouse.release(Button.left)
 
-def loop(mouse, speed_coeficient):
+
+def scroll_down(mouse):
+    mouse.scroll(0, 4 * random.randint(1, 4))
+
+
+def scroll_up(mouse):
+    mouse.scroll(0, -4 * random.randint(1, 4))
+
+
+def loop(mouse, speed_coeficient, scroll):
     interval = random.randint(3, 10) / speed_coeficient
     click(mouse)
     time.sleep(interval)
 
+    if scroll:
+        if random.randint(0, 10) == 7:
+            scroll_down(mouse)
+        elif random.randint(0, 10) == 4:
+            scroll_up(mouse)
+
+
 def main():
     if(len(sys.argv) == 1):
         speed_coeficient = 4
+    elif (len(sys.argv) == 2):
+        speed_coeficient = int(sys.argv[1])
+        scroll = False    
     else:
         speed_coeficient = int(sys.argv[1])
-    print('[ARGS]', 'speed_coeficient = ', speed_coeficient)
+        scroll = bool(sys.argv[2])
+    print('[ARGS]', 'speed_coeficient = ', speed_coeficient, '; scroll =', scroll)
     mouse = Controller()
     while True:
-        loop(mouse, speed_coeficient)
+        loop(mouse, speed_coeficient, scroll)
 
 if __name__ == "__main__":
     main()
